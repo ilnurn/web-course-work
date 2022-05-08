4c686848dec810659d08810e765b28710ec626cc
Ilnur <nabiulli@mail.ru>
2022-05-08 20:35:20 +0500

    Первый коммит

---
+++ src/main/java/pro/sky/java/course2/webcoursework/WebCourseWorkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.webcoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCourseWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCourseWorkApplication.class, args);
+	}
+
+}
